4b59223fe35c3ee0e0eab2b0e8cc9c102ced3fdf
Marcus Almeida Reis <marcusrall@Mac-mini-de-Marcus.local>
2024-01-26 23:07:39 -0300

    Foi criada a estrutura de component e iniciado a tela de Home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <>
-      <header>
-        <UserButton afterSignOutUrl="/" />
-      </header>
-      <div>Your home page's content can go here.</div>
+      <h1 className="head-text text-left">Home</h1>
     </>
   );
 }
